106397d344afbb87925142207911e12ec054e115
patil <patilvedant0108@gmail.com>
2023-08-03 17:13:21 +0530

    frontend changes and pass enc testt

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, request, jsonify
 import cx_Oracle
 from cryptography.fernet import Fernet
 app = Flask(__name__)
+key = Fernet.generate_key()
+fernet = Fernet(key)
 
 def get_connection():
     return cx_Oracle.connect('hr/hr@localhost:1521/xe')
@@ -11,9 +13,46 @@ def index():
     return render_template("index.html")
 
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        entered_password = request.form.get('password')
+        codedpwd = fernet.encrypt(entered_password.encode())
+
+        try:
+            # Establish the database connection
+            conn = get_connection()
+            cursor = conn.cursor()
+
+            # Fetch the user data based on the entered email
+            sql = "SELECT email, USER_PASSWORD FROM SIGNUP WHERE email = :1"
+            cursor.execute(sql, (email,))
+            user_data = cursor.fetchone()
+
+            if user_data:
+                # Verify the entered password against the stored hashed password
+                stored_password = user_data[1]
+                if codedpwd == stored_password:
+                    return jsonify({'message': 'Login successful!'})
+                else:
+                    return jsonify({'error': 'Invalid email or password'})
+            else:
+                return jsonify({'error': 'Invalid email or password'})
+
+        except Exception as e:
+            return jsonify({'error': str(e)})
+
+        finally:
+            if cursor:
+                cursor.close()
+            if conn:
+                conn.close()
+                print("connection closed")
+
+        # If the request is a GET request, render the login page
     return render_template("login.html")
+
 
 
 @app.route('/payment')
@@ -29,8 +68,6 @@ def signup():
         mobile = request.form.get('Mobnum')
         email = request.form.get('Email')
         encpass = request.form.get('pwd')
-        key = Fernet.generate_key()
-        fernet = Fernet(key)
         user_password = fernet.encrypt(encpass.encode())
 
 
