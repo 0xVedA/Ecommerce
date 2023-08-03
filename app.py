1a5036178ea15ca154ca3975cb60064324def226
patil <patilvedant0108@gmail.com>
2023-08-03 10:11:01 +0530

    Database connection established and we can write data into database

--- app.py
+++ app.py
@@ -1,30 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 import cx_Oracle
 app = Flask(__name__)
 
-try:
-    con = cx_Oracle.connect('hr/hr@localhost:1521/xe')
-    print(con.version)
-
-    cursor = con.cursor()
-
-    #cursor.execute('create table "CART"("UID" integer primary key, "NAME" varchar2(30), "ITEM" varchar2(100))')
-    #cursor.execute('''insert into CART values (001, 'YT', 'TV')''')
-    con.commit()
-    print("Table Inserted successfully")
-
-except cx_Oracle.DatabaseError as e:
-    print("There is a problem with Oracle", e)
-
-finally:
-    if cursor:
-        cursor.close()
-    if con:
-        con.close()
+def get_connection():
+    return cx_Oracle.connect('hr/hr@localhost:1521/xe')
 
 @app.route('/')
 def index():
     return render_template("index.html")
+
 
 @app.route('/login')
 def login():
@@ -35,16 +19,40 @@ def login():
 def payment():
     return render_template("paymentform.html")
 
-@app.route('/signup')
+
+@app.route('/signup', methods=['GET', 'POST'])
 def signup():
+    if request.method == 'POST':
+        # Get the form data submitted by the user
+        name = request.form.get('Uname')
+        mobile = request.form.get('Mobnum')
+        email = request.form.get('Email')
+        user_password = request.form.get('pwd')
+
+        try:
+            con = get_connection()
+            cursor = con.cursor()
+
+            sql = "INSERT INTO SIGNUP (UNAME, mobile, email, USER_PASSWORD) VALUES (:1, :2, :3, :4)"
+            cursor.execute(sql, (name, mobile, email, user_password))
+            con.commit()
+            print("values inserted")
+
+        except Exception as e:
+            return jsonify({'error': str(e)})
+
+        finally:
+            if cursor:
+                cursor.close()
+            if con:
+                con.close()
+                print("connection closed")
+
+        # Return a response to the user after the form data is processed
+        return jsonify({'message': 'User registered successfully!'})
+
     return render_template("signup.html")
 
 
-
-
-
-
-
 if __name__ == "__main__":
-    app.run()
-#vedant
+    app.run(debug=True)
